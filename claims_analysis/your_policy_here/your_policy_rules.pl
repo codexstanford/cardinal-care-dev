47a5e56d11d777dda6ec57cd6d7d2f0c6f1f6806
labhinav <abhinavlalwani@gmail.com>
2024-01-27 18:31:13 -0800

    add rules for physical visit limit

--- claims_analysis/your_policy_here/your_policy_rules.pl
+++ claims_analysis/your_policy_here/your_policy_rules.pl
@@ -19,10 +19,10 @@ policy_active(C,P):-
   hospitalization.end_time(H,H_END_DATE,H_END_TIME) &
   policy.startdate(P,Cl,P_START_DATE) &
   policy.enddate(P,Cl,P_END_DATE) &
-  get_timestamp(H_START_DATE,H_START_STAMP) &
-  get_timestamp(H_END_DATE,H_END_STAMP) &
-  get_timestamp(P_END_DATE,P_END_STAMP) &
-  get_timestamp(P_START_DATE,P_START_STAMP) &
+  get_timestamp_from_date(H_START_DATE,H_START_STAMP) &
+  get_timestamp_from_date(H_END_DATE,H_END_STAMP) &
+  get_timestamp_from_date(P_END_DATE,P_END_STAMP) &
+  get_timestamp_from_date(P_START_DATE,P_START_STAMP) &
   leq(P_START_STAMP,H_START_STAMP) &
   leq(H_END_STAMP,P_END_STAMP)
 
@@ -34,12 +34,12 @@ valid_hospitalization(C,P):-
   eligible_service(C,P,R) &
   ~exception(C,P).
 
-get_timestamp(DATE,TIME,STAMP) :-
+get_timestamp_from_datetime(DATE,TIME,STAMP) :-
   evaluate(parsedate(DATE),[D,M,Y]) &
   evaluate(parsetime(TIME),[HR,MIN]) &
   evaluate(maketimestamp(Y,M,D,HR,MIN,0),STAMP)
 
-get_timestamp(DATE,STAMP) :-
+get_timestamp_from_date(DATE,STAMP) :-
   evaluate(parsedate(DATE),[D,M,Y]) &
   evaluate(maketimestamp(Y,M,D,0,0,0),STAMP)
 
@@ -51,40 +51,39 @@ exception(C,P):-
   person.occupation(armed_forces) &
   claim.consequence_of_occupation(C,yes).
 
-eligible_service(C,P,routine_physical).
+eligible_service(C,P,routine_physical):-
+  claim.claimant(C,Cl)
+  person.age(Cl,Age) &
+  physical_visit_limit(Age,Limit) &
+  evaluate(countofall(X,previous_physical_visit(C,X),Count)) &
+  evaluate(minus(Limit,1),LimitMinus) &
+  leq(Count,LimitMinus).
+
+physical_visit_limit(Age,Limit):-
+  physical_visit_limit(MinAge,MaxAge,Limit) &
+  evaluate(minus(MaxAge,1),MaxAgeMinus) &
+  leq(Age,MaxAgeMinus) &
+  leq(MinAge,Age).
+
+previous_physical_visit(Claim1,Claim2):-
+  claim.claimant(Claim1,Person) &
+  claim.claimant(Claim2,Person) &
+  claim.hospitalization(Claim2,Hosp2) &
+  hospitalization.reason(Hosp2,routine_physical) &
+  hospitalization.start_time(Hosp2,Hosp2_StartDate,Hosp2_StartTime) &
+  policy_year_startdate(Policy_StartDate) &
+  get_timestamp_from_datetime(Hosp2_StartDate,Hosp2_StartTime,Hosp2_Timestamp) &
+  get_timestamp_from_date(Policy_StartDate,Policy_Timestamp) &
+  leq(Policy_Timestamp,Hosp2_Timestamp)
+
+
+
 eligible_service(C,P,preventive_care).
 valid_hospital(stanford_medical_center).
 valid_hospital(menlo_medical_clinic).
 valid_hospital(sutter_health).
-
-# age_greater_than(P,Y,C):-
-#   person.birthdate(P,BIRTHDATE) &
-#   get_timestamp(BIRTHDATE,BIRTHTIMESTAMP) &
-#   claim.hospitalization(C,H) &
-#   hospitalization.end_time(H,ENDDATE,ENDTIME) &
-#   get_timestamp(ENDDATE,ENDTIME,ENDTIMESTAMP) &
-#   evaluate(times(31536000000,Y),YEARTIMESTAMP) &
-#   plus(BIRTHTIMESTAMP,YEARTIMESTAMP,MINTIMESTAMP) &
-#   leq(MINTIMESTAMP,ENDTIMESTAMP).
-
-# age_lesser_than(P,Y,C):-
-#   person.birthdate(P,BIRTHDATE) &
-#   get_timestamp(BIRTHDATE,BIRTHTIMESTAMP) &
-#   claim.hospitalization(C,H) &
-#   hospitalization.start_time(H,STARTDATE,STARTTIME) &
-#   get_timestamp(STARTDATE,STARTTIME,STARTTIMESTAMP) &
-#   evaluate(times(31536000000,Y),YEARTIMESTAMP) &
-#   plus(BIRTHTIMESTAMP,YEARTIMESTAMP,MAXTIMESTAMP) &
-#   leq(STARTTIMESTAMP,MAXTIMESTAMP).
-
-# eligible_service(C,P,routine_physical):-
-#   claim.claimant(C,Cl) &
-#   age_greater_than(Cl,22,C) &
-#   no_previous_visit_in_policy_year(C,P).
-
-# no_previous_visit_in_policy_year(C,P):-
-#   person.hospitalization(P,H) &
-#   hospitalization.
+physical_visit_limit(0,22,0).
+physical_visit_limit(22,200,1)
 
 
 
