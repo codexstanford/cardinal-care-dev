cc642bb83f0604d8408cad6051a5081289168c64
Harshit Gupta <mail4harshit1@gmail.com>
2024-01-30 03:28:34 -0800

    corrected bugs

--- claims_analysis/your_policy_here/your_policy_rules.pl
+++ claims_analysis/your_policy_here/your_policy_rules.pl
@@ -54,21 +54,25 @@ exception(C,P):-
 eligible_service(C,P,routine_physical):-
   claim.claimant(C,Cl) &
   person.dob(Cl,DOB) &
-  claim.time(Claim2,C2_D,C2_T) &
-  evaluate(minus(C2_D,DOB),Age) &
+  claim.time(C,C_D,C_T) &
+  evaluate(parsedate(C_D),[D,M,Y]) &
+  evaluate(parsedate(DOB),[D2,M2,Y2]) &
+  evaluate(minus(Y,Y2),Age) &
   physical_visit_limit(Age,Limit) &
-  evaluate(countofall(X,physical_visit_current_year(C,X)) + 1,Count) &
+  evaluate(plus(countofall(X,physical_visit_current_year(C,X)),1),Count) &
   leq(Count,Limit).
 
 eligible_service(C,P,preventive_care):-
   claim.claimant(C,Cl) &
   person.dob(Cl,DOB) &
-  claim.time(Claim2,C2_D,C2_T) &
-  evaluate(minus(C2_D,DOB),Age) &
+  claim.time(C,C_D,C_T) &
+  evaluate(parsedate(C_D),[D,M,Y]) &
+  evaluate(parsedate(DOB),[D2,M2,Y2]) &
+  evaluate(minus(Y,Y2),Age) &
   claim.hospitalization(C,H) &
   hospitalization.vaccine(H,V) &
   preventive_care_limit(V,Age,Limit,MinAge,MaxAge) &
-  evaluate(countofall(X,preventive_care_visit(C,X)) + 1,Count) &
+  evaluate(plus(countofall(X,preventive_care_visit(C,X)),1),Count) &
   leq(Count,Limit).
 
 preventive_care_limit(Vaccine,Age,Limit,MinAge,MaxAge):-
@@ -88,7 +92,6 @@ physical_visit_current_year(Claim1,Claim2):-
   claim.claimant(Claim2,Person) &
   claim.policy(Claim1,Policy) &
   claim.policy(Claim2,Policy) &
-  covered(Claim2) &
   claim.hospitalization(Claim2,Hosp2) &
   claim.hospitalization(Claim1,Hosp1) &
   hospitalization.reason(Hosp1,routine_physical) &
@@ -105,14 +108,14 @@ physical_visit_current_year(Claim1,Claim2):-
   claim.time(Claim2,C2_D,C2_T) &
   get_timestamp_from_datetime(C1_D,C1_T,C1_TS) &
   get_timestamp_from_datetime(C2_D,C2_T,C2_TS) &
-  lt(C2_TS,C1_TS).
+  lt(C2_TS,C1_TS) &
+  covered(Claim2)
 
 preventive_care_visit(Claim1,Claim2):-
   claim.claimant(Claim1,Person) &
   claim.claimant(Claim2,Person) &
   claim.policy(Claim1,Policy) &
   claim.policy(Claim2,Policy) &
-  covered(Claim2) &
   claim.hospitalization(Claim2,Hosp2) &
   claim.hospitalization(Claim1,Hosp1) &
   hospitalization.reason(Hosp1,preventive_care) &
@@ -122,15 +125,19 @@ preventive_care_visit(Claim1,Claim2):-
   claim.time(Claim1,C1_D,C1_T) &
   claim.time(Claim2,C2_D,C2_T) &
   person.dob(Person,DOB) &
-  evaluate(minus(C1_D,DOB),Age1) &
-  evaluate(minus(C2_D,DOB),Age2) &
+  evaluate(parsedate(C1_D),[D1,M1,Y1]) &
+  evaluate(parsedate(C2_D),[D2,M2,Y2]) &
+  evaluate(parsedate(DOB),[D_dob,M_dob,Y_dob]) &
+  evaluate(minus(Y1,Y_dob),Age) &
+  evaluate(minus(Y2,Y_dob),Age2) &
   preventive_care_limit(Vaccine,Age1,Limit,MinAge,MaxAge) &
   evaluate(minus(MaxAge,1),MaxAgeMinus) &
   leq(Age2,MaxAgeMinus) &
   leq(MinAge,Age2) &
   get_timestamp_from_datetime(C1_D,C1_T,C1_TS) &
   get_timestamp_from_datetime(C2_D,C2_T,C2_TS) &
-  lt(C2_TS,C1_TS).
+  lt(C2_TS,C1_TS) &
+  covered(Claim2)
   
 
 policy_year_startdate(01_08_2023).
@@ -149,5 +156,7 @@ preventive_care_limit(tb,24,200,0).
 
 lt(X,Y):-
   leq(X,Y) &
-  neq(X,Y).
+  ~same(X,Y).
 
+definition(parsedate(DATE),map(readstring,tail(matches(stringify(DATE),"(..)_(..)_(....)"))))
+definition(parsetime(TIME),map(readstring,tail(matches(stringify(TIME),"(..)_(..)"))))
