82d56d21d231384753a252312447348f76be4f55
pjames27 <44187608+pjames27@users.noreply.github.com>
2024-02-09 12:04:18 -0800

    Implemented localStorage Epilog Datasets
    - Added src/user_data_storage.js, which has four simple functions for saving, loading, clearing, and printing datasets in localStorage.
        - Currently, these methods only allow the saving and loading of a single dataset at a time. This is meant to emulate the current structure of saving and loading files on the deployed server, which assumes each user has precisely one dataset and cannot access information in any others, except for the common data.

---
+++ src/user_data_storage.js
@@ -0,0 +1,27 @@
+// Dependencies: epilog.js
+
+const CURR_USER_KEY = "../../../user_datasets/current_user.hdf";
+
+// Return type: an epilog dataset
+function getCurrentUserDataset() {
+    return definemorefacts([], readdata(localStorage[CURR_USER_KEY]));
+}
+
+// Accepts an epilog dataset and saves it to localStorage as text
+function setCurrentUserDataset(newDataset) {
+    localStorage[CURR_USER_KEY] = grindem(newDataset);
+}
+
+// Deletes the user dataset localStorage
+function clearCurrentUserDataset() {
+    localStorage.removeItem(CURR_USER_KEY);
+}
+
+function printCurrentUserDataset() {
+    if (localStorage.getItem(CURR_USER_KEY) === null) {
+        console.log("NO CURRENT USER DATASET FOUND");
+        return;
+    }
+    
+    console.log(localStorage[CURR_USER_KEY]);
+}
