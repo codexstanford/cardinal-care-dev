e40f3391c33445fe3752f51c7aebe11cac2b92b6
pjames27 <44187608+pjames27@users.noreply.github.com>
2024-05-14 14:41:57 -0700

    Resolved #47

--- src/test_running.js
+++ src/test_running.js
@@ -1,7 +1,16 @@
 // Depends on epilog.js
 
-// Returns true if the query run on the dataset (induced by dataset_str) and ruleset produces exactly the results in expected_result_list (ignoring order)
+// Returns true if the query run on the dataset (induced by dataset_str) and the ruleset produces exactly the results in expected_result_list (ignoring order)
     // and false otherwise
+
+// Parameters:
+    // test_name (string): a string that will be used to refer to the test when debug messages are printed to the console
+    // variable_expr (string): the name of the variable in query_str that will be matched for results
+    // query_str (string): the query to run on the dataset and ruleset
+    // expected_result_list (array<string>): the values that variable_expr is expected to match with. The results of the query when run on the dataset and ruleset should exactly match this list (ignoring order).
+    // dataset_str (string): a string specifying the Epilog dataset that will be queried
+    // ruleset (epilog ruleset): the ruleset that will be queried
+    // verbose (boolean): if true, prints additional debug information to the console. If false, only prints whether the test failed or succeeded.
 
 function run_unit_test(test_name, variable_expr, query_str, expected_result_list, dataset_str, ruleset, verbose = false) {
     let query = read(query_str);
